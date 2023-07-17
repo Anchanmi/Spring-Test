607933755cc64bc28b5d8dada4011d2aa2e21dec
Chanmi An <bangoul345@gmail.com>
2023-07-17 17:58:07 +0900

    커스텀 메서드(init, destroy)

--- sp5-chap06/src/main/java/config/AppCtx.java
+++ sp5-chap06/src/main/java/config/AppCtx.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.Client;
+import spring.Client2;
 
 @Configuration
 public class AppCtx {
@@ -12,5 +13,12 @@ public class AppCtx {
 		Client client = new Client();
 		client.setHost("host");
 		return client;
+	}
+	
+	@Bean(initMethod = "connect", destroyMethod = "close")
+	public Client2 client2() {
+		Client2 client2 = new Client2();
+		client2.setHost("host");
+		return client2;
 	}
 }
