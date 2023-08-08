d4fcced45c0f2ae0b099ce3ff6b43a087f3682e1
Chanmi An <bangoul345@gmail.com>
2023-08-08 16:35:22 +0900

    step1 -> step2로 넘어가는 코드 추가

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterController {
@@ -9,5 +11,13 @@ public class RegisterController {
 	@RequestMapping("/register/step1")
 	public String handleStep1() {
 		return "register/step1";
+	}
+	
+	@PostMapping("/register/step2")
+	public String handlestep2(@RequestParam(value = "agree", defaultValue = "false") Boolean agreeVal) {
+		if(!agreeVal) {
+			return "register/step1";
+		}
+		return "register.step2";
 	}
 }
