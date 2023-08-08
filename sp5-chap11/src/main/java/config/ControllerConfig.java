d88ce302cfed0d757d2b51f4463c5698a29b9c34
Chanmi An <bangoul345@gmail.com>
2023-08-08 19:09:34 +0900

    설문조사 컨트롤러 추가

--- sp5-chap11/src/main/java/config/ControllerConfig.java
+++ sp5-chap11/src/main/java/config/ControllerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
 import controller.RegisterController;
+import survey.SurveyController;
 
 @Configuration
 public class ControllerConfig {
@@ -11,6 +12,11 @@ public class ControllerConfig {
 	@Bean
 	public RegisterController registerController() {
 		return new RegisterController();
+	}
+	
+	@Bean
+	public SurveyController surveyController() {
+		return new SurveyController();
 	}
 	
 }
