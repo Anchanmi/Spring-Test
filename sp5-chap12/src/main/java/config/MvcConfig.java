c9eb412a70358e502689ce7d58e4a2ce0fa5ca20
Chanmi An <bangoul345@gmail.com>
2023-08-10 15:22:04 +0900

    오타 수정(lable -> label)

--- sp5-chap12/src/main/java/config/MvcConfig.java
+++ sp5-chap12/src/main/java/config/MvcConfig.java
@@ -32,7 +32,7 @@ public class MvcConfig implements WebMvcConfigurer{
 	@Bean
 	public MessageSource messageSource() {
 		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
-		ms.setBasename("message.lable");
+		ms.setBasename("message.label");
 		ms.setDefaultEncoding("UTF-8");
 		return ms;
 	}
