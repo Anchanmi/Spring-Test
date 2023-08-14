399396979c3029f090c7c7c52f5f29ded569087b
Chanmi An <bangoul345@gmail.com>
2023-08-14 20:03:05 +0900

    autowired 추가(자동 의존주입)

--- sp5-chap13/src/main/java/controller/ChangePwdController.java
+++ sp5-chap13/src/main/java/controller/ChangePwdController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import spring.*;
 
@@ -15,6 +16,7 @@ import spring.*;
 @RequestMapping("/edit/changePassword")
 public class ChangePwdController {
 	
+	@Autowired
 	private ChangePasswordService changePasswordService;
 	
 	public void setChangePasswordService(ChangePasswordService changePasswordService) {
