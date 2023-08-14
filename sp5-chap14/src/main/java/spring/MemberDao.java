ab677c2e7e3e1e017efdfa2334167a6411404371
Chanmi An <bangoul345@gmail.com>
2023-08-14 20:35:56 +0900

    회원 가입 일자를 기준으로 검색하는 기능 구현

--- sp5-chap14/src/main/java/spring/MemberDao.java
+++ sp5-chap14/src/main/java/spring/MemberDao.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
+import java.time.LocalDateTime;
 
 import javax.sql.DataSource;
 
@@ -55,6 +56,24 @@ public class MemberDao {
 				return member;
 			}
 		});
+		return results;
+	}
+	
+	public List<Member> selectByRegdate(LocalDateTime from, LocalDateTime to){
+		List<Member> results = jdbcTemplate.query("select * from MEMBER where REGDATE between ? and ? " +
+													"order by REGDATE desc", 
+				new RowMapper<Member>() {
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException{
+				Member member = new Member(
+						rs.getString("EMAIL"),
+						rs.getString("PASSWORD"),
+						rs.getString("NAME"),
+						rs.getTimestamp("REGDATE").toLocalDateTime());
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+		}, from, to);
 		return results;
 	}
 	
