f223a61d3de8fbbcc8153f2f2c9f586830021139
Chanmi An <bangoul345@gmail.com>
2023-08-14 21:32:35 +0900

    @ExceptionHandler를 메서드에 적용하여 컨트롤러에서 발생한 익셉션 처리

--- sp5-chap14/src/main/java/controller/MemberDetailController.java
+++ sp5-chap14/src/main/java/controller/MemberDetailController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.TypeMismatchException;
+
 
 import spring.*;
 
@@ -25,5 +28,15 @@ public class MemberDetailController {
 		}
 		model.addAttribute("member", member);
 		return "member/memberDetail";
+	}
+	
+	@ExceptionHandler(TypeMismatchException.class)
+	public String handleTypeMismatchException() {
+		return "member/invalidId";
+	}
+	
+	@ExceptionHandler(MemberNotFoundException.class)
+	public String handleNotFoundException() {
+		return "member/noMember";
 	}
 }
