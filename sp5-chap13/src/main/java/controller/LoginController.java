4be31dfe15f92d72155e1f5c035919653ae7a573
Chanmi An <bangoul345@gmail.com>
2023-08-14 19:35:57 +0900

    세션 추가

--- sp5-chap13/src/main/java/controller/LoginController.java
+++ sp5-chap13/src/main/java/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
 
 import spring.AuthInfo;
 import spring.AuthService;
@@ -28,13 +29,14 @@ public class LoginController {
 	}
 	
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors) {
+	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if(errors.hasErrors()) {
 			return "login/loginForm";
 		}
 		try {
 			AuthInfo authInfo = authService.authenticate(loginCommand.getEmail(), loginCommand.getPassword());
+			session.setAttribute("authInfo", authInfo);
 			return "login/loginSuccess";
 			
 		}catch(WrongIdPasswordException e) {
