af028294a84d5b294b286df41dc77f210ad6ab2f
Chanmi An <bangoul345@gmail.com>
2023-08-14 19:23:25 +0900

    @Autowirde 추가

--- sp5-chap13/src/main/java/controller/LoginController.java
+++ sp5-chap13/src/main/java/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import spring.AuthInfo;
 import spring.AuthService;
@@ -14,6 +15,7 @@ import spring.WrongIdPasswordException;
 @RequestMapping("/login")
 public class LoginController {
 
+	@Autowired
 	private AuthService authService;
 	
 	public void setAuthService(AuthService authService) {
