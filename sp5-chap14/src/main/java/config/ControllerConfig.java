7aeb40fe8524e8a50c3094687e3f513dc2e6dd18
Chanmi An <bangoul345@gmail.com>
2023-08-14 20:31:36 +0900

    chap14(날짜 값 변환, @PathVariable, 익셉션 처리) 준비로 chap13예제 그대로 갖고 오기

---
+++ sp5-chap14/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,39 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import controller.RegisterController;
+import controller.LoginController;
+import controller.LogoutController;
+import controller.ChangePwdController;
+import survey.SurveyController;
+
+@Configuration
+public class ControllerConfig {
+
+	@Bean
+	public RegisterController registerController() {
+		return new RegisterController();
+	}
+	
+	@Bean
+	public SurveyController surveyController() {
+		return new SurveyController();
+	}
+	@Bean
+	public LoginController loginController() {
+		return new LoginController();
+	}
+	
+	@Bean
+	public LogoutController logoutController() {
+		return new LogoutController();
+	}
+	
+	@Bean
+	public ChangePwdController changePwdController() {
+		return new ChangePwdController();
+	}
+	
+}
