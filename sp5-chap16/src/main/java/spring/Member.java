d95d9cc77e305f150779814f6ce045c34550ce70
Chanmi An <bangoul345@gmail.com>
2023-08-16 17:33:53 +0900

    JsonFormat 주석처리

--- sp5-chap16/src/main/java/spring/Member.java
+++ sp5-chap16/src/main/java/spring/Member.java
@@ -3,8 +3,8 @@ package spring;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import com.fasterxml.jackson.annotation.JsonFormat;
+//import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+//import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class Member {
 	
@@ -13,7 +13,7 @@ public class Member {
 	@JsonIgnore
 	private String password;
 	private String name;
-	@JsonFormat(shape = Shape.STRING)
+	//@JsonFormat(shape = Shape.STRING)
 	private LocalDateTime registerDateTime;
 	
 	public Member(String email, String password, String name, LocalDateTime regDateTime) {
